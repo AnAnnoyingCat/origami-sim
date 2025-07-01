ddbf6bb020ce650cfcc4588c74899a41f4c9569b
AnAnnoyingCat home-desktop <cwernke@student.ethz.ch>
2025-07-01 09:51:51 +0200

    create friction force and stiffness skeletons

---
+++ src/assemble_ground_friction_stiffness.cpp
@@ -0,0 +1,9 @@
+#include "assemble_ground_friction_stiffness.h"
+
+void assemble_ground_friction_stiffness(Eigen::SparseMatrix<double> &K, Eigen::Ref<const Eigen::VectorXd> q, double min_barrier_distance) {
+	
+}
+
+void get_friction_stiffness_for_vertex(Eigen::SparseMatrix<double> &K, const Eigen::Vector3d q0, double d) {
+
+}
